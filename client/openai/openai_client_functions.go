ce424af05feb6deda8b6360d628782c97690f1ea
dro14 <doniyorbek752@gmail.com>
2023-06-29 20:03:33 +0500

    fix 400 from openai

--- client/openai/openai_client_functions.go
+++ client/openai/openai_client_functions.go
@@ -26,27 +26,23 @@ func send(ctx context.Context, request *types.Request) (*http.Response, error) {
 	switch resp.StatusCode {
 	case http.StatusOK:
 		return resp, nil
-	case http.StatusBadRequest,
-		http.StatusTooManyRequests,
-		http.StatusBadGateway,
-		http.StatusServiceUnavailable:
+	case http.StatusBadGateway, http.StatusServiceUnavailable:
 		return nil, fmt.Errorf("%s for %d", resp.Status, userID)
 	default:
 		bts, _ := io.ReadAll(resp.Body)
-		var response types.Response
 		_ = resp.Body.Close()
-
+		var response types.Response
 		err = json.Unmarshal(bts, &response)
-		if err != nil {
-			log.Printf("%s for %d\ncan't decode response: %v\nbody: %q", resp.Status, userID, err, string(bts))
-		} else {
-			log.Printf("%s for %d\ntype: %s\nmessage: %s", resp.Status, userID, response.Error.Type, response.Error.Message)
-		}
 
 		switch response.Error.Type {
 		case e.InvalidRequest:
 			return nil, fmt.Errorf(response.Error.Type + response.Error.Message)
 		default:
+			if err != nil {
+				log.Printf("%s for %d\ncan't decode response: %v\nbody: %q", resp.Status, userID, err, string(bts))
+			} else {
+				log.Printf("%s for %d\ntype: %s\nmessage: %s", resp.Status, userID, response.Error.Type, response.Error.Message)
+			}
 			return nil, fmt.Errorf("%s for %d", resp.Status, userID)
 		}
 	}
