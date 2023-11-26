98db1fc3ffef385facb5c84ee3b013285044563c
dro14 <doniyorbek752@gmail.com>
2023-11-26 13:26:45 +0500

    disable translation

--- processor/processor_process.go
+++ processor/processor_process.go
@@ -50,7 +50,7 @@ func (p *Processor) Process(ctx context.Context, message *tgbotapi.Message, isPr
 	var completion string
 	var completions []string
 	channel := make(chan string)
-	if lang(ctx) == "uz" && isPremium == "false" {
+	if lang(ctx) == "" && isPremium == "false" {
 		message.Text = p.apis.Translate("auto", "en", message.Text)
 		ctx = context.WithValue(ctx, "stream", false)
 		go p.openai.ProcessCompletions(ctx, message.Text, msg, channel)
