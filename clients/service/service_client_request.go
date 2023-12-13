478c86bc2c0f4cd5fa148d4ab484ce8f3fd86aad
dro14 <doniyorbek752@gmail.com>
2023-12-13 13:35:21 +0500

    add timeout and retrying to service requests

--- clients/service/service_client_request.go
+++ clients/service/service_client_request.go
@@ -7,6 +7,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/dro14/yordamchi/utils"
 )
 
 func (s *Service) makeRequest(ctx context.Context, request map[string]any, url string) (map[string]any, error) {
@@ -26,9 +30,20 @@ func (s *Service) makeRequest(ctx context.Context, request map[string]any, url s
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
+	client.Timeout = 10 * time.Second
+	retryDelay := utils.RetryDelay
+	attempts := 0
+Retry:
+	attempts++
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("can't do request:", err)
+		if strings.Contains(err.Error(), "context deadline exceeded") {
+			if attempts < utils.RetryAttempts {
+				utils.Sleep(&retryDelay)
+				goto Retry
+			}
+		}
 		return nil, err
 	}
 	defer func() { _ = resp.Body.Close() }()
