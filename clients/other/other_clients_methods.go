32934a18a78719f1c4113f16ec4d52fcd50bcc63
dro14 <doniyorbek752@gmail.com>
2024-04-29 18:35:41 +0500

    disable code translation

--- clients/other/other_clients_methods.go
+++ clients/other/other_clients_methods.go
@@ -61,40 +61,44 @@ func (o *APIs) OCR(ctx context.Context, photoURL, caption string) string {
 }
 
 func (o *APIs) Translate(sl, tl, q string) string {
-	qs := utils.Slice(q, 5000)
+	parts := strings.Split(q, "```")
+	for i := 0; i < len(parts); i += 2 {
+		qs := utils.Slice(parts[i], 5000)
 
-	for i := range qs {
-		resp, err := http.Get(fmt.Sprintf(o.translateURL, sl, tl, url.QueryEscape(qs[i])))
-		if err != nil {
-			log.Println("can't do request:", err)
-			return strings.Join(qs, " ")
+		for j := range qs {
+			resp, err := http.Get(fmt.Sprintf(o.translateURL, sl, tl, url.QueryEscape(qs[j])))
+			if err != nil {
+				log.Println("can't do request:", err)
+				return strings.Join(qs, " ")
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				log.Println("bad status:", resp.Status)
+				return strings.Join(qs, " ")
+			}
+
+			bts, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("can't read body:", err)
+				return strings.Join(qs, " ")
+			}
+
+			_, after, found := strings.Cut(string(bts), "<div class=\"result-container\">")
+			if !found {
+				log.Println("not found")
+				return strings.Join(qs, " ")
+			}
+
+			translation, _, found := strings.Cut(after, "</div>")
+			if !found {
+				log.Println("not found")
+				return strings.Join(qs, " ")
+			}
+
+			qs[j] = html.UnescapeString(translation)
 		}
-
-		if resp.StatusCode != http.StatusOK {
-			log.Println("bad status:", resp.Status)
-			return strings.Join(qs, " ")
-		}
-
-		bts, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Println("can't read body:", err)
-			return strings.Join(qs, " ")
-		}
-
-		_, after, found := strings.Cut(string(bts), "<div class=\"result-container\">")
-		if !found {
-			log.Println("not found")
-			return strings.Join(qs, " ")
-		}
-
-		translation, _, found := strings.Cut(after, "</div>")
-		if !found {
-			log.Println("not found")
-			return strings.Join(qs, " ")
-		}
-
-		qs[i] = html.UnescapeString(translation)
+		parts[i] = strings.Join(qs, " ")
 	}
 
-	return strings.Join(qs, " ")
+	return strings.Join(parts, "```")
 }
