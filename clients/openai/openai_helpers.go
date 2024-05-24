fdd191d648c846208d6acc7d740717c6ce81f004
dro14 <doniyorbek752@gmail.com>
2024-05-24 23:35:37 +0500

    fix bug with nil assignment

--- clients/openai/openai_helpers.go
+++ clients/openai/openai_helpers.go
@@ -81,8 +81,7 @@ func streamResponse(ctx context.Context, resp *http.Response, channel chan<- str
 
 	stream.Store(false)
 	response.Choices[0].Message.Role = response.Choices[0].Delta.Role
-	if response.Choices[0].Delta.ToolCalls != nil {
-		response.Choices[0].Message.ToolCalls = response.Choices[0].Delta.ToolCalls
+	if response.Choices[0].Message.ToolCalls != nil {
 		response.Choices[0].Message.ToolCalls[0].Function.Arguments = args.String()
 	}
 	response.Choices[0].Message.Content = content.String()
