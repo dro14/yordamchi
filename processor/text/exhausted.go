8e8d30d282c08801cc089853fab0ab7ac63e5f88
dro14 <doniyorbek752@gmail.com>
2024-05-24 15:32:59 +0500

    fix exhausted message text

--- processor/text/exhausted.go
+++ processor/text/exhausted.go
@@ -1,7 +1,7 @@
 package text
 
 var Exhausted = map[string]string{
-	"uz": `*Siz 50ta bepul so'rovlaringizni ishlatib bo'ldingiz. Pullik tariflar bilan ko'proq so'rovlarga ega bo'ling:*`,
-	"ru": `*Вы исчерпали свой лимит 50 бесплатных запросов. С платными подписками, получите больше запросов:*`,
-	"en": `*You have exhausted your limit of 50 free requests. Get more requests with paid subscriptions:*`,
+	"uz": `*Afsuski, Sizda so'rovlar qolmadi. Pullik tariflar bilan ko'proq so'rovlarga ega bo'ling:*`,
+	"ru": `*К сожалению, у Вас не осталось запросов. С платными подписками, получите больше запросов:*`,
+	"en": `*Unfortunately, You have no requests left. Get more requests with paid subscriptions:*`,
 }
