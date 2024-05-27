ae76f173ea061ac84517f547640d818760d8ed8e
dro14 <doniyorbek752@gmail.com>
2024-05-27 19:47:20 +0500

    edit formating of search header

--- processor/text/search.go
+++ processor/text/search.go
@@ -1,7 +1,7 @@
 package text
 
 var Search = map[string]string{
-	"uz": "\n\n### %s QIDIRUV NATIJALARI:\n\n",
-	"ru": "\n\n### РЕЗУЛЬТАТЫ ПОИСКА %s:\n\n",
-	"en": "\n\n### %s SEARCH RESULTS:\n\n",
+	"uz": "\n\n*%s QIDIRUV NATIJALARI:*\n\n",
+	"ru": "\n\n*РЕЗУЛЬТАТЫ ПОИСКА %s:*\n\n",
+	"en": "\n\n*%s SEARCH RESULTS:*\n\n",
 }
